a74ca477477136fbbc4f5f9fd5a4d19de1949cf8
David Runge <dave@sleepmap.de>
2019-01-21 22:17:52 +0100

    .travis/before-script-linux.sh: Setting script strict. Conditionally selecting cmake arguments from array'ed sets, based on environment variables. Quoting variables.

--- .travis/before-script-linux.sh
+++ .travis/before-script-linux.sh
@@ -1,9 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-$TRAVIS_BUILD_DIR/.travis/lint.sh $TRAVIS_BUILD_DIR
-if [[ -n "$1" && "$1" == "--qt=true" ]]; then
-	source /opt/qt59/bin/qt59-env.sh
-	cmake -DSC_EL=OFF -DCMAKE_INSTALL_PREFIX:PATH=$TRAVIS_BUILD_DIR/BUILD/Install -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR --debug-output
+set -eou pipefail
+
+default_cmake_options=(
+"-DCMAKE_INSTALL_PREFIX:PATH=$TRAVIS_BUILD_DIR/BUILD/Install"
+'-DCMAKE_BUILD_TYPE=Release' '-DSC_EL=OFF' )
+qt_off_options=( '-DSC_QT=OFF' '-DSC_IDE=OFF' )
+system_boost_options=( '-DSYSTEM_BOOST=ON' )
+system_yamlcpp_options=( '-DSYSTEM_YAMLCPP=ON' )
+
+"$TRAVIS_BUILD_DIR/.travis/lint.sh" "$TRAVIS_BUILD_DIR"
+
+if [ -n "$QT" ]; then
+  # shellcheck disable=SC1091
+  source /opt/qt59/bin/qt59-env.sh
 else
-	cmake -DSC_EL=OFF -DSC_QT=OFF -DSC_IDE=OFF -DCMAKE_INSTALL_PREFIX:PATH=$TRAVIS_BUILD_DIR/BUILD/Install -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR --debug-output
+  default_cmake_options+=( "${qt_off_options[@]}" )
 fi
+
+if [ -n "$SYSTEM_BOOST" ]; then
+  default_cmake_options+=( "${system_boost_options[@]}" )
+fi
+
+if [ -n "$SYSTEM_YAMLCPP" ]; then
+  default_cmake_options+=( "${system_yamlcpp_options[@]}" )
+fi
+
+cmake "${default_cmake_options[@]}" "$TRAVIS_BUILD_DIR" --debug-output
