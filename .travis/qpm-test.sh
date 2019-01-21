eae1a8aed79082bb7699ac9a5f24fe4cd7471744
David Runge <dave@sleepmap.de>
2019-01-21 22:16:36 +0100

    .travis/qpm-test.sh: Setting script strict. Quoting variables.

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks
+set -eou pipefail
+
+qpm test.run -l "$TRAVIS_BUILD_DIR/BUILD/travis_test_run.json" --path "$SCLANG" --include "$HOME/Quarks"
