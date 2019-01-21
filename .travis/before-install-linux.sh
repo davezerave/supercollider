ac76ab6c5d3b7a9afca7834f6453d7d887a504be
David Runge <dave@sleepmap.de>
2019-01-21 22:14:57 +0100

    .travis/before-install-linux.sh: Separating all functionality into functions. Making the script strict and conditionally adding to the default packages to be installed based on environment variables, set by travis.

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -1,12 +1,46 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-npm install -g lintspaces-cli
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository --yes ppa:beineri/opt-qt591-trusty
-sudo apt-get update
-sudo apt-get install --yes build-essential gcc-4.9 g++-4.9 cmake pkg-config libjack-jackd2-dev libsndfile1-dev libasound2-dev libavahi-client-dev libreadline6-dev libfftw3-dev libicu-dev libxt-dev libudev-dev
-if [[ -n "$1" && "$1" == "--qt=true" ]]; then
-	sudo apt-get install --yes libgl1-mesa-dev qt59base qt59location qt59declarative qt59tools qt59webengine qt59webchannel qt59xmlpatterns qt59svg qt59websockets
-fi
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
-sudo update-alternatives --auto gcc
+set -eou pipefail
+
+additional_repositories=( 'ppa:ubuntu-toolchain-r/test'
+'ppa:beineri/opt-qt591-trusty' )
+default_packages=( 'build-essential' 'gcc-4.9' 'g++-4.9' 'cmake' 'pkg-config'
+'libjack-jackd2-dev' 'libsndfile1-dev' 'libasound2-dev' 'libavahi-client-dev'
+'libreadline6-dev' 'libfftw3-dev' 'libicu-dev' 'libxt-dev' 'libudev-dev' )
+qt_packages=( 'libgl1-mesa-dev' 'qt59base' 'qt59location' 'qt59declarative'
+'qt59tools' 'qt59webengine' 'qt59webchannel' 'qt59xmlpatterns' 'qt59svg'
+'qt59websockets' )
+boost_packages=( 'boost-all-dev' )
+yamlcpp_packages=( 'libyaml-cpp-dev' )
+
+add_additional_repositories() {
+  for repo in "${additional_repositories[@]}"; do
+    sudo add-apt-repository --yes "${repo}"
+  done
+  sudo apt-get update
+}
+
+install_packages() {
+  local packages=( "${default_packages[@]}" )
+  if [ -n "$QT" ]; then
+    packages+=( "${qt_packages[@]}" )
+  fi
+  if [ -n "$SYSTEM_BOOST" ]; then
+    packages+=( "${boost_packages[@]}" )
+  fi
+  if [ -n "$SYSTEM_YAMLCPP" ]; then
+    packages+=( "${yamlcpp_packages[@]}" )
+  fi
+  npm install -g lintspaces-cli
+  sudo apt-get install --yes "${packages[@]}"
+}
+
+update_alternatives() {
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 \
+                           --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
+  sudo update-alternatives --auto gcc
+}
+
+add_additional_repositories
+install_packages
+update_alternatives
